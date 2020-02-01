302891469f9ef885a94c05b0d1e9f38de43714f0
Gage Pickle <gagepickle@yahoo.com>
2020-01-31 22:04:01 -0600

    Added variables to index.js and Updated package.json

---
+++ index.js
@@ -0,0 +1,5 @@
+const name = "Gage"; // constant
+let string = "This is a string"; // string variable
+let boolean = true; // boolean variable
+let number = 22; // number variable
+string = 2; // converting string to a number
