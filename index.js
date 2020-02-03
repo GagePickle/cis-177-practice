d5b940f7e0afb05e0b113bfa33c03a4a1eac1929
Gage Pickle <gagepickle@yahoo.com>
2020-02-03 13:44:54 -0600

    Added examples of implicit and explicit coercion along with parses

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
-const name = "Gage"; // constant
-let numOfYearsCoding = 1;
-// + is a CONCATENATION OPERATOR.
-// Whenever STRINGS are present, '+' means CONCATENATE.
-// console.log(
-//   "My name is " +
-//     name +
-//     " and I have been coding for " +
-//     numOfYearsCoding +
-//     " years."
-// );
-// Template Literals cleaner and easier way
-console.log(
-  `My name is ${name} and I have been coding for ${numOfYearsCoding} years`
-);
-console.log(); // not an error but does not do anything because no argument
+// JS will COERCE your data types into whatever it feels is appropriate.
+// IMPLICIT COERCION
+console.log("Hello" + 22);
+console.log(22 + 22);
+console.log(22 + true);
+
+// Explicit Coercion
+console.log(Number(false));
+console.log(String(22));
+console.log(Boolean(222));
+console.log("3" * 3);
+console.log("asdwads" * 3);
+
+// Can be used to convert data entered into forms from string to numbers
+console.log(parseFloat("123.45"));
+console.log(parseInt("123.45"));
