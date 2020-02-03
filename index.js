26a3a652fbee3a5268f55dd68f92da7cca78640c
Gage Pickle <gagepickle@yahoo.com>
2020-02-03 15:53:43 -0600

    Added answers to questions

--- index.js
+++ index.js
@@ -1,16 +1,24 @@
-// JS will COERCE your data types into whatever it feels is appropriate.
-// IMPLICIT COERCION
-console.log("Hello" + 22);
-console.log(22 + 22);
-console.log(22 + true);
-
-// Explicit Coercion
-console.log(Number(false));
-console.log(String(22));
-console.log(Boolean(222));
-console.log("3" * 3);
-console.log("asdwads" * 3);
-
-// Can be used to convert data entered into forms from string to numbers
-console.log(parseFloat("123.45"));
-console.log(parseInt("123.45"));
+// create a variable "timeOfDay" and assign it a string
+let timeOfDay = "Afternoon";
+// create a variable "greeting" that references a template literal
+// use "timeOfDay" in the template literal to create a message like "Good morning!" or "Good evening!"
+let greeting = `Good ${timeOfDay}`;
+// print "greeting"
+console.log(greeting);
+// create a new variable, but do not assign it a value
+let x;
+// print the variable and its type
+console.log(typeof x);
+// ? what type should we expect? undefined
+// assign the variable a value that indicates the variable is purposely blank
+x = null;
+// ? what value should we assign? null
+// print the variable and its type again
+console.log(typeof x);
+// ? what type should we expect? object
+// try to print a variable that does not exist
+console.log(y);
+// ? what should we expect to print in the CLI? an undefined error
+// print "greeting" again
+console.log(greeting);
+// ? will this line run? no because there is an error before it
