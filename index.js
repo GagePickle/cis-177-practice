c690767dbad1851a7f8ba342091efb44e726067b
Gage Pickle <gagepickle@yahoo.com>
2020-02-03 13:16:05 -0600

    Add example of template literals and interpolation

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
 const name = "Gage"; // constant
-let numOfYearsCoding = 9;
-// Accessing an OBJECT called 'console' and using DOT NOTATION to access a KEY .
-// That KEY is a METHOD, so we INVOKE it with ().
-// We also pass in an ARGUMENT as a STRING.
-// ARGUMENTS provide additional information that a METHOD needs to do its job.
-console.log("Hello World!");
+let numOfYearsCoding = 1;
+// + is a CONCATENATION OPERATOR.
+// Whenever STRINGS are present, '+' means CONCATENATE.
+// console.log(
+//   "My name is " +
+//     name +
+//     " and I have been coding for " +
+//     numOfYearsCoding +
+//     " years."
+// );
+// Template Literals cleaner and easier way
+console.log(
+  `My name is ${name} and I have been coding for ${numOfYearsCoding} years`
+);
 console.log(); // not an error but does not do anything because no argument
