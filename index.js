36a10e651119dd7c2dfef6be9b813556c575d702
Gage Pickle <gagepickle@yahoo.com>
2020-02-03 13:04:55 -0600

    Added notes on objects, methods, and arguments

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const name = "Gage"; // constant
-let string = "This is a string"; // string variable
-let boolean = true; // boolean variable
-let number = 22; // number variable
-string = 2; // converting string to a number
+let numOfYearsCoding = 9;
+// Accessing an OBJECT called 'console' and using DOT NOTATION to access a KEY .
+// That KEY is a METHOD, so we INVOKE it with ().
+// We also pass in an ARGUMENT as a STRING.
+// ARGUMENTS provide additional information that a METHOD needs to do its job.
+console.log("Hello World!");
+console.log(); // not an error but does not do anything because no argument
