436286e2c4f6a984586261e2f2dc3f53324bf214
Gage Pickle <gagepickle@yahoo.com>
2020-02-10 13:40:07 -0600

    Add exmaple of if-else

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
-// Relation or COMPARISON OPERATORS always yield BOOLEANS
-console.log(1 === 2);
+const x = 3;
 
-// To check is 2 things are considered to be equal...
-// '===' is STRICT EQUALITY
-// It hecks the VALUE and TYPE
-console.log(2 === "2"); // false
-console.log(2 === 2); // true
-console.log(2 === 3); // false
-console.log(2 !== 3); // true
+// an EXPRESSION is anything that evaluates to a value
+// STATEMENTS combine EXPRESSIONS with keywords and other syntax
+// {}s establish scope of the code.
+if (x === 3) {
+  console.log("x is 3!");
+} else {
+  console.log("x is not 3!");
+}
+
+const age = 25;
+
+if (age >= 25) {
+  console.log("Renting a car is affordable");
+} else if (age >= 18) {
+  console.log("Renting a car will be expensive!");
+} else {
+  console.log("You can't rent a car!");
+}
