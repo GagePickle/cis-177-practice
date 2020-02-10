8e9a5d3c0f174395141ec0e15e35cb407b204d38
Gage Pickle <gagepickle@yahoo.com>
2020-02-10 13:23:47 -0600

    Add examples of comparison operators

--- index.js
+++ index.js
@@ -1,29 +1,10 @@
-// Nested object
-const me = {
-  name: "Gage",
-  age: 22,
-  major: "Web Development",
-  features: {
-    eyes: "Green",
-    hair: "Black",
-    alive: true
-  }
-};
-console.log(me);
-// Add key value pair using dot notation
-me.features.gender = "Male";
-// Change values of key value pairs
-me.name = "Gage Pickle";
-me.features.hair = "Brown";
-// Log values of nested object
-console.log(me.features.gender);
-console.log(me.features.hair);
-console.log(me.features.alive);
-// Bracket notation
-// use BRACKET NOTATION to access a key that is REFERENCED by a VARIABLE.
-const keyThatINeed = "hair";
-// Template literal using bracket notation and dot notation
-console.log(`Bracket notation with variable:`, me.features[keyThatINeed]);
-console.log(`Bracket notation with string: ${me["keyThatINeed"]}`); // undefined
-// console.log(me[keyThatINeed][hair]); // you can chain with bracket notation
-console.log(me["keyThatINeed"]); // undefined
+// Relation or COMPARISON OPERATORS always yield BOOLEANS
+console.log(1 === 2);
+
+// To check is 2 things are considered to be equal...
+// '===' is STRICT EQUALITY
+// It hecks the VALUE and TYPE
+console.log(2 === "2"); // false
+console.log(2 === 2); // true
+console.log(2 === 3); // false
+console.log(2 !== 3); // true
