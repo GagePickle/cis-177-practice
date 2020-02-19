e41d7f9458138f4387a6f39144585337ec28cbc4
Gage Pickle <gagepickle@yahoo.com>
2020-02-19 13:31:31 -0600

    Add Fizz Buzz solution using unary operator

--- index.js
+++ index.js
@@ -6,15 +6,26 @@
 
 let x = 0;
 
-while (x < 100) {
-  x += 1;
-  if (x % 3 === 0 && x % 5 === 0) {
+while (x <= 100) {
+  // (!0)
+  if (!(x % 3) && !(x % 5)) {
     console.log(x + " FizzBuzz!");
-  } else if (x % 3 === 0) {
+  } else if (!(x % 3)) {
     console.log(x + " Fizz!");
-  } else if (x % 5 === 0) {
+  } else if (!(x % 5)) {
     console.log(x + " Buzz!");
   } else {
     console.log(x);
   }
+  x += 1;
 }
+
+// '!' is the NOT operator. It's a UNARY OPERATOR
+// Using this in front of any data type (such as a number)
+// Will immediately convert that data type into a Boolean
+// Then that value will be inverted as per the NOT operator
+console.log(Boolean(0));
+console.log(!111);
+console.log(!"Hello");
+console.log(!"");
+console.log(!true);
