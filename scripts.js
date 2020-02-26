1b2b12ce6413725e83f893c441a3413932f08d26
Gage Pickle <gagepickle@yahoo.com>
2020-02-26 12:42:03 -0600

    Add FizzBuzz using for and example of confirm

--- scripts.js
+++ scripts.js
@@ -1,6 +1,32 @@
-const item = {};
+// Log the numbers from 1 to 10.
+// In parentheses, specify 3 things - start - stop - increment
+for (let i = 1; i <= 10; i++) {
+  console.log(i);
+}
 
-// Prompt is a BLOCKING operation.
-item.name = prompt("What the item's name?");
-item.price = prompt("What is the item's value?");
-alert(`${item.name} is worth ${item.price}`);
+let total = 0;
+
+for (let i = 1; i <= 10; i++) {
+  total += i;
+  console.log(total);
+}
+
+for (let i = 1; i <= 100; i++) {
+  if (i % 3 === 0 && i % 5 === 0) {
+    console.log("FizzBuzz!");
+  } else if (i % 3 === 0) {
+    console.log("Fizz!");
+  } else if (i % 5 === 0) {
+    console.log("Buzz!");
+  } else {
+    console.log(i);
+  }
+}
+
+const x = {};
+
+if (window.confirm("Okay or cancel?")) {
+  x.isconfirmed = true;
+} else {
+  x.isconfirmed = false;
+}
